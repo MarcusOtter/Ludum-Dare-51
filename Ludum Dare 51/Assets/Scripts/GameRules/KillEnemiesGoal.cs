24faa32d511a4c5032a53cd64b8923d98eb8f6cb
Dan <44238640+grommithub@users.noreply.github.com>
2022-10-01 22:38:31 +0200

    stuff

--- Ludum Dare 51/Assets/Scripts/GameRules/KillEnemiesGoal.cs
+++ Ludum Dare 51/Assets/Scripts/GameRules/KillEnemiesGoal.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KillEnemies", menuName = "ScriptableObjects/GameGoals/KillEnemies", order = 1)]
 public class KillEnemiesGoal : GameGoal
 {
+    public int PointsPerKill = 1;
     public override void OnEnter()
     {
         base.OnEnter();
