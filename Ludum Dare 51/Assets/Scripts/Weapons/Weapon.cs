119e052ed036d5bf6da5999092c453a7a9fa8b01
MarcusOtter <github@otterstrom.dev>
2022-10-02 19:09:26 +0200

    Implement weapon shooting

--- Ludum Dare 51/Assets/Scripts/Weapons/Weapon.cs
+++ Ludum Dare 51/Assets/Scripts/Weapons/Weapon.cs
@@ -1,21 +1,36 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody), typeof(FollowTransform))]
 public abstract class Weapon : MonoBehaviour
 {
 	[Header("Base weapon settings")]
-	[SerializeField] protected float damage;
-	[SerializeField] protected float fireDelayInSeconds;
-	[SerializeField] protected float spread;
+	[SerializeField] protected int damage = 1;
+	[SerializeField] protected float fireDelayInSeconds = 0.5f;
+	[SerializeField] public bool isAutomatic;
+	[SerializeField] protected SoundEffect attackSound;
 
+	public Action<Weapon> OnAttack;
 	protected Rigidbody Rigidbody;
 	
 	private FollowTransform _followTransform;
+
+	private float _lastFireTime;
 	
 	protected virtual void Awake()
 	{
 		Rigidbody = GetComponent<Rigidbody>();
 		_followTransform = GetComponent<FollowTransform>();
+	}
+
+	public virtual void Attack()
+	{
+		if (!CanAttack()) return;
+		
+		SoundEffectPlayer.PlaySoundEffect(attackSound, transform);
+		_lastFireTime = Time.time;
+		OnAttack?.Invoke(this);
 	}
 
 	public virtual void PickUp(Transform weaponHolderTransform)
@@ -29,5 +44,10 @@ public abstract class Weapon : MonoBehaviour
 		transform.position = newPosition;
 		_followTransform.SetTarget(null);
 		Rigidbody.isKinematic = false;
+	}
+	
+	protected virtual bool CanAttack()
+	{
+		return Time.time >= _lastFireTime + fireDelayInSeconds;
 	}
 }
